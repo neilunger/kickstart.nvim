6c1c7d8b8ecb5a5f1f5836cd6efb953018d9a804
neilunger <31961516+neilunger@users.noreply.github.com>
2025-03-13 22:14:09 -0500

    Add bash and verilog formatters

--- init.lua
+++ init.lua
@@ -769,8 +769,10 @@ require('lazy').setup({
         --
         -- You can use 'stop_after_first' to run the first available formatter from the list
         -- javascript = { "prettierd", "prettier", stop_after_first = true },
+        bash = { 'shfmt' },
         python = { 'ruff' },
         systemverilog = { 'verible' },
+        verilog = { 'verible' },
         vhdl = { 'vsg' },
       },
     },
